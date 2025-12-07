97692c2517569e8acd3f6bf048cda57103d9349b
Matt Lloyd Davies <103773518+Turzum@users.noreply.github.com>
2025-12-07 17:31:16 +0000

    Correct typo in sleep function
    Fix typo in 'time.sleep' function call.

--- auto-typer.py
+++ auto-typer.py
@@ -14,5 +14,6 @@ with open(cmdFilename, 'r') as f:
 
 for command in commands:
     pyautogui.write(command, interval=keystrokeInterval)
+    tiem.sleep(1)
     pyautogui.press('enter')
     time.sleep(postCommandDelay)
